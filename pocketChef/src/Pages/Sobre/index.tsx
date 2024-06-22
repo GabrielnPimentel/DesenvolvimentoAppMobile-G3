26d4d10727dcf31ec2433caa94c091d70773752c
vittorio <vittfassano@gmail.com>
2024-06-22 17:01:31 -0300

    fix: edit sobre page

--- pocketChef/src/Pages/Sobre/index.tsx
+++ pocketChef/src/Pages/Sobre/index.tsx
@@ -1,13 +1,58 @@
-import React from 'react'
-import { View, Text } from 'react-native' 
-import { styles } from './style'
+import React from "react";
+import { View, Text, Image, ScrollView } from "react-native";
+import { styles } from "./style";
 
 const Sobre = () => {
+  const chefs = [
+    {
+      id: 1,
+      name: "Hattori Hanzo",
+      description:
+        "Hattori Hanzo é um mestre chef nascido em Okinawa e criado em Xangai, cujos olhos serenos e mãos precisas escondem o segredo de um lendário forjador de espadas samurais. Sua habilidade transcende a culinária, imbuindo cada prato e lâmina com uma perfeição quase sobrenatural.",
+      image: require("../../assets/hattorihanzo.jpg"),
+    },
+    {
+      id: 2,
+      name: "Chef 2",
+      description: "Descrição do Chef 2.",
+      image: require("../../assets/hattorihanzo.jpg"),
+    },
+    {
+      id: 3,
+      name: "Chef 3",
+      description: "Descrição do Chef 3.",
+      image: require("../../assets/hattorihanzo.jpg"),
+    },
+    {
+      id: 4,
+      name: "Chef 4",
+      description: "Descrição do Chef 4.",
+      image: require("../../assets/hattorihanzo.jpg"),
+    },
+    {
+      id: 5,
+      name: "Chef 5",
+      description: "Descrição do Chef 5.",
+      image: require("../../assets/hattorihanzo.jpg"),
+    },
+  ];
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.titulo}>Sobre</Text>
-    </View>
-  )
-}
+    <ScrollView style={styles.scrollView}>
+      <View style={styles.container}>
+        <Text style={styles.titulo}>Sobre</Text>
+        {chefs.map((chef) => (
+          <View key={chef.id} style={styles.chefContainer}>
+            <Image source={chef.image} style={styles.chefImage} />
+            <View style={styles.chefInfo}>
+              <Text style={styles.chefName}>{chef.name}</Text>
+              <Text style={styles.chefDescription}>{chef.description}</Text>
+            </View>
+          </View>
+        ))}
+      </View>
+    </ScrollView>
+  );
+};
 
 export default Sobre;
--- pocketChef/src/Pages/Sobre/style.ts
+++ pocketChef/src/Pages/Sobre/style.ts
@@ -1,17 +1,49 @@
 import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
+  scrollView: {
+    flex: 1,
+    backgroundColor: "#70B9BE",
+  },
   container: {
     alignItems: "center",
     width: "100%",
-    height: "100%",
-    backgroundColor: "#70B9BE",
+    paddingVertical: 20,
+    paddingHorizontal: 10,
   },
   titulo: {
     textAlign: "center",
-    paddingTop: 300,
+    marginBottom: 20,
     fontSize: 30,
     fontWeight: "bold",
     color: "#fff",
+  },
+  chefContainer: {
+    flexDirection: "row",
+    width: "100%",
+    backgroundColor: "#daf7f7",
+    padding: 15,
+    borderRadius: 10,
+    marginTop: 20,
+    alignItems: "center",
+  },
+  chefImage: {
+    width: 120,
+    height: 120,
+    borderRadius: 60,
+  },
+  chefInfo: {
+    marginLeft: 15,
+    flex: 1,
+  },
+  chefName: {
+    fontSize: 18,
+    fontWeight: "bold",
+    color: "#333",
+  },
+  chefDescription: {
+    marginTop: 5,
+    fontSize: 12,
+    color: "#383838",
   },
 });
