d05aec4938cd18a066b69171b862cf36a26249c6
Gabrits <162373342+Gabrits@users.noreply.github.com>
2024-06-25 19:11:37 -0300

    fix: trocando assets por Assets
    subindo de uma vez para não prejudicar o codigo

--- pocketChef/src/Pages/Sobre/index.tsx
+++ pocketChef/src/Pages/Sobre/index.tsx
@@ -9,31 +9,31 @@ const Sobre = () => {
       name: "Hattori Hanzo",
       description:
         "Hattori Hanzo é um mestre chef nascido em Okinawa e criado em Xangai, cujos olhos serenos e mãos precisas escondem o segredo de um lendário forjador de espadas samurais. Sua habilidade transcende a culinária, imbuindo cada prato e lâmina com uma perfeição quase sobrenatural.",
-      image: require("../../assets/hattorihanzo.jpg"),
+      image: require("../../Assets/hattorihanzo.jpg"),
     },
     {
       id: 2,
       name: "Alfredo Linguine",
       description: "Chef Linguini é um jovem e desajeitado cozinheiro em ascensão no renomado restaurante Gusteau's em Paris. Apesar de sua falta de habilidades culinárias, ele forma uma parceria improvável com Remy, um rato talentoso na cozinha. Juntos, eles criam pratos excepcionais que conquistam os críticos e clientes.",
-      image: require("../../assets/linguine.jpeg"),
+      image: require("../../Assets/linguine.jpeg"),
     },
     {
       id: 3,
       name: "Tia Nastácia",
       description: "Tia Nastácia é a amável e talentosa cozinheira do Sítio do Picapau Amarelo, conhecida por seus deliciosos pratos da culinária brasileira e bolos caseiros. Suas receitas, como feijoada e bolos de fubá, conquistam todos que visitam o sítio. Sua habilidade culinária e carinho na cozinha são parte essencial da magia e acolhimento do lugar.",
-      image: require("../../assets/anastacia.jpeg"),
+      image: require("../../Assets/anastacia.jpeg"),
     },
     {
       id: 4,
       name: "Carmen Carmy Berzatto",
       description: "Reconhecido por sua maestria na culinária americana, Carmy é admirado por sua capacidade de criar pratos que combinam técnicas tradicionais com sabores contemporâneos. Seu menu inclui desde costelas defumadas suculentas até hambúrgueres gourmet e tortas de maçã feitas com carinho.",
-      image: require("../../assets/carmen.jpeg"),
+      image: require("../../Assets/carmen.jpeg"),
     },
     {
       id: 5,
       name: "Richard Slowik",
       description: "Richard Slowik é um chef renomado na culinária inglesa. Conhecido por sua abordagem meticulosa e criativa, Slowik transforma ingredientes simples em pratos sofisticados que capturam a essência da gastronomia britânica.",
-      image: require("../../assets/slowik.jpeg"),
+      image: require("../../Assets/slowik.jpeg"),
     },
   ];
 
